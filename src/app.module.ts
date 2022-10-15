d46e9266d491d95483829b3fdd5e7365f4982469
olliso17 <olli.so17@gmail.com>
2022-10-15 00:24:38 -0300

    finalizando estudo com nest

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,31 @@
-import { Module } from '@nestjs/common';
+import {  Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config/dist/config.module';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { ProdutosController } from './produtos.controller';
-import { ProdutosService } from './produtos.services';
+import { LivrosController } from './livros.controller';
+import { LivrosService } from './livros.services';
+import { SequelizeModule } from '@nestjs/sequelize';
+import * as pg from 'pg';
+import { Livro } from './livro.model';
 
 @Module({
-  imports: [],
-  controllers: [AppController, ProdutosController],
-  providers: [AppService, ProdutosService],
+  imports: [
+    ConfigModule.forRoot(),
+    SequelizeModule.forRoot({
+      dialect: 'postgres',
+      dialectModule: pg,
+      host: 'localhost',
+      port: 5432,
+      username: process.env.USUARIO_BANCO_DADOS,
+      password: process.env.SENHA_BANCO_DADOS,
+      database: 'postgres',
+      autoLoadModels: true,
+      synchronize:true
+    }),
+    SequelizeModule.forFeature([Livro])
+   
+  ],
+  controllers: [AppController, LivrosController],
+  providers: [AppService, LivrosService],
 })
-export class AppModule {}
+export class AppModule { }
